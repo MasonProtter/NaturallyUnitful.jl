0eec707114f4ecfdf5ebbb4a75d9909cdbdebc71
Mason Protter <mason.protter@gmail.com>
2019-01-24 13:43:43 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test, NaturallyUnitful
 @testset "tests" begin 
     @test natural(1u"m") == 5.067730759202785e6u"eV^-1"
 
-    @test unnatural(u"m", ans) == 1.0u"m"
+    @test unnatural(u"m", 5.067730759202785e6u"eV^-1") == 1.0u"m"
 
     @test natural(1e8u"m/s") == 0.33356409519815206
 
