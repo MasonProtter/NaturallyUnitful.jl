84ab935b3e91bbbc2ad86f47df7e19f60c48b107
MasonProtter <mason.protter@gmail.com>
2019-05-22 12:44:04 -0600

    added documentation and support for natural bases

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,10 @@ using Test, NaturallyUnitful
     @test natural(1e8u"m/s") == 0.33356409519815206
 
     @test unnatural(u"m/s", 0.33356409519815206) == 1e8u"m/s"
+
+    @test natural(1u"kg", base=u"GeV") ==  5.609588650020685e26u"GeV"
+    
+    @test natural(1u"m", base=u"GeV") ==  5.067730759202785e15u"GeV^-1"
+
+    @test unnatural(u"K", 1u"eV") == 11604.522060401006u"K" 
 end
